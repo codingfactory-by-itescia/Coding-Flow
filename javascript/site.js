6b069559ef8d6c712bdb74e7488111b447b25bad
robLittiere <robinl.95@orange.fr>
2020-11-19 09:13:51 +0100

    Commit frontend
    Commit frontend

--- javascript/site.js
+++ javascript/site.js
@@ -5,3 +5,12 @@ a.addEventListener('click',displayTags);
 function displayTags(){
     a.style = "block;"
 };
+
+
+
+function getValue() {
+    // Sélectionner l'élément input et récupérer sa valeur
+    var input = document.getElementById("in").value;
+    // Afficher la valeur
+    alert(input);
+}
